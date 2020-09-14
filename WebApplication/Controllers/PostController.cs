e1f1d699bc954bb6cff96ee048d9df9a57d39321
marcioeric <marcio.eric.nicolau@gmail.com>
2020-09-13 23:50:44 -0300

    Início da arquitetura com ember js.

---
+++ WebApplication/Controllers/PostController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    [ApiController]
+    [Route("api/posts")]
+    public class PostController : Controller
+    {
+        public IActionResult Index(int page)
+        {
+            return Ok(new {title = "Olá mundo!"});
+        }
+    }
+}
